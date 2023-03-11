7713202893e90338bedae5dba97890e9ce151898
Solomon Botchway <62394255+snnbotchway@users.noreply.github.com>
2023-03-11 23:33:58 +0000

    Revert "Implement authentication"

--- b2b/conftest.py
+++
@@ -1,9 +0,0 @@
-"""Global project fixtures."""
-import pytest
-from rest_framework.test import APIClient
-
-
-@pytest.fixture
-def api_client():
-    """Return API client."""
-    return APIClient()
--- b2b/user/apps.py
+++
@@ -1,9 +0,0 @@
-"""User app configuration."""
-from django.apps import AppConfig
-
-
-class UserConfig(AppConfig):
-    """User app configuration class."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "user"

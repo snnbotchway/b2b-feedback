ffb00fc3c59dd7784deefcd76a9c8c0fadc9cd63
snnbotchway <solomonbotchway7@gmail.com>
2023-03-12 19:57:25 +0000

    Configure Questionnaire Admin

--- b2b/b2b/settings.py
+++ b2b/b2b/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "core.apps.CoreConfig",
     "feedback.apps.FeedbackConfig",
     # Third-party apps
+    "nested_admin",
     "rest_framework",
     "rest_framework.authtoken",
 ]
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- b2b/feedback/admin.py
+++ b2b/feedback/admin.py
@@ -1,32 +1,43 @@
 """Account app admin configurations."""
 from django.contrib import admin
+from nested_admin.nested import NestedModelAdmin, NestedStackedInline
 
-from .models import Client
+from .models import Client, Question, QuestionChoice, Questionnaire
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     """Define admin configuration for the Client model."""
 
-    autocomplete_fields = [
-        "client_rep",
-        "sales_manager",
-    ]
-    list_display = [
-        "email",
-        "name",
-        "client_rep",
-        "sales_manager",
-        "id",
-    ]
-    list_editable = [
-        "name",
-    ]
-    list_select_related = [
-        "client_rep",
-        "sales_manager",
-    ]
-    search_fields = [
-        "email__icontains",
-        "name__icontains",
-    ]
+    autocomplete_fields = ["client_rep", "sales_manager"]
+    list_display = ["email", "name", "client_rep", "sales_manager", "id"]
+    list_editable = ["name"]
+    list_select_related = ["client_rep", "sales_manager"]
+    search_fields = ["email__icontains", "name__icontains"]
+
+
+class QuestionChoiceInline(NestedStackedInline):
+    """Inline class for question choices."""
+
+    model = QuestionChoice
+    extra = 0
+
+
+class QuestionInline(NestedStackedInline):
+    """Inline class for questionnaire questions."""
+
+    model = Question
+    inlines = [QuestionChoiceInline]
+    extra = 0
+
+
+@admin.register(Questionnaire)
+class QuestionnaireAdmin(NestedModelAdmin):
+    """Define admin configuration for the Questionnaire model."""
+
+    autocomplete_fields = ["client_rep"]
+    inlines = [QuestionInline]
+    list_display = ["title", "client_rep", "created_at", "due_at", "id"]
+    list_editable = ["due_at"]
+    list_select_related = ["client_rep"]
+    search_fields = ["title__icontains", "description__icontains"]

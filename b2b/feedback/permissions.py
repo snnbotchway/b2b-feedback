5ce2c707d4199498486b2fcc0aa93593807a3d5a
snnbotchway <solomonbotchway7@gmail.com>
2023-03-12 21:07:09 +0000

    Add client list questionnaires functionality

--- b2b/feedback/permissions.py
+++ b2b/feedback/permissions.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import Group
 from rest_framework.permissions import BasePermission
 
-from .models import SALES_MANAGER_GROUP
+from .models import CLIENT_REP_GROUP, SALES_MANAGER_GROUP
 
 
 class IsSalesManager(BasePermission):
@@ -12,3 +12,12 @@ class IsSalesManager(BasePermission):
         """Return true if current user is in the Sales Mangers group."""
         sales_manager_group, _ = Group.objects.get_or_create(name=SALES_MANAGER_GROUP)
         return sales_manager_group in request.user.groups.all()
+
+
+class IsClientRepresentative(BasePermission):
+    """Permission class to check if a user is in the Client Representatives group."""
+
+    def has_permission(self, request, view):
+        """Return true if current user is in the Client Representatives group."""
+        client_rep_group, _ = Group.objects.get_or_create(name=CLIENT_REP_GROUP)
+        return client_rep_group in request.user.groups.all()

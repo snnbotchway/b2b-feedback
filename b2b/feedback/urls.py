528642d91b1cd7a8989b8adea46e3ddd11b405e1
snnbotchway <solomonbotchway7@gmail.com>
2023-03-12 19:55:44 +0000

    Build create questionnaire API

--- b2b/feedback/urls.py
+++ b2b/feedback/urls.py
@@ -1,10 +1,11 @@
 """Feedback app url configuration."""
 from rest_framework.routers import DefaultRouter
 
-from .views import ClientViewSet
+from .views import ClientViewSet, QuestionnaireViewSet
 
 router = DefaultRouter()
 router.register("clients", ClientViewSet)
+router.register("questionnaires", QuestionnaireViewSet)
 
 app_name = "feedback"
 

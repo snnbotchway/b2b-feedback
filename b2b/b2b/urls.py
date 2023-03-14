e9492c0a8923ad7aa9352bc3092490a28d916888
snnbotchway <solomonbotchway7@gmail.com>
2023-03-14 20:07:36 +0000

    Merge react build with django

--- b2b/b2b/urls.py
+++ b2b/b2b/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html")),
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
     path("feedback/", include("feedback.urls")),
